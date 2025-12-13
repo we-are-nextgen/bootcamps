8011c385a19df7ba2dd4466ab76c4d750a804030
wael2000 <waelibrahim2000@hotmail.com>
2025-12-13 21:52:51 +0100

    Done

---
+++ load.sh
@@ -0,0 +1,5 @@
+curl http://localhost:8080/import/bootcamp/openshift
+curl http://localhost:8080/import/bootcamp/kubernetes
+curl http://localhost:8080/import/bootcamp/containers
+curl http://localhost:8080/import/bootcamp/nextjs
+curl http://localhost:8080/import/bootcamp/quarkus
